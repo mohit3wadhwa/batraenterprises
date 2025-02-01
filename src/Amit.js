c503bf2ba7bb6743ae0a6b0c7f7aea01adbfa72a
Mohit Wadhwa <mohit3wadhwa@gmail.com>
2025-02-02 00:15:28 +0530

    Update Amit.js

--- src/Amit.js
+++ src/Amit.js
@@ -15,7 +15,7 @@ export const products = [
     },
     {
       id: 3,
-      name: 'Product 3x - Wowwwwwwwwwwwwww! Github Actions Works',
+      name: 'Product 3x - Wowwwwwwwwwwwwwww! Github Actions Works',
       imageUrl: './images/3.JPG',
       description: 'This is another great product.',
       price: '$20.00',
